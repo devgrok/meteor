2869baf159949e4d41b9bcbddc9975bd96c431a0
Chris Watts <devgrok@users.noreply.github.com>
2015-12-23 17:27:18 +1100

    attempts to create a track for meteor

--- packages/meteor-tool/package.js
+++ packages/meteor-tool/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "The Meteor command-line tool",
-  version: '1.1.11-dev',
+  version: '1.1.11_2',
   name: 'devgrok:meteor-tool',
   git: "https://github.com/devgrok/meteor.git",
 });
